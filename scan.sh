a6351ff39545c1efb813ae96e0d7c64cad7c187d
nhduc <duc.nguyenhoang@axonactive.com>
2019-08-09 11:51:57 +0700

    scan script

---
+++ scan.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+base=$1
+
+if [ -z $base ]; then
+    echo "Please provide base path as paramter!";
+    exit;
+fi;
+
+if [ -d $base ]; then
+    find $base -name "*.java" | grep -v "\/unittest\/" > scan_out
+else
+    echo "Given folder '" $base "' does not exists!"
+    exit;
+fi;
+
