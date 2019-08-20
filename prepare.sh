a5c8054b7a05d2db43cfaac8e432523af8206e0e
Oz <xad.love@gmail.com>
2019-08-20 21:09:23 +0700

    prepare for run

---
+++ prepare.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+base=$1
+
+if [ -z $base ]; then
+    echo "Please provide base path as paramter!";
+    exit;
+fi;
+
+ufold="$base/unittest";
+bfold="$base/jmocktest";
+
+if [ -d $ufold ]; then
+    echo "found test folder at: $ufold";
+    if [ -d $bfold ]; then
+        echo "Clean up $bfold"
+        rm -rf $bfold;
+    fi;
+    cp -R $ufold $bfold
+    for i in `find $bfold -type f`; do
+        rm $i;
+    done;
+    find $ufold -type f -iname "*Test.java" > uts
+else
+    echo "Given folder $base doesn't have folder 'unittest' as direct child";
+    exit;
+fi;
+
