ac7c0d4abecbdf8543e347b14c90802c706f04fe
Oz <xad.love@gmail.com>
2019-08-29 22:21:55 +0700

    complete fact based verify

--- src/main/java/worker/VerifyWorker.java
+++ src/main/java/worker/VerifyWorker.java
@@ -1,8 +1,11 @@
 package worker;
 
+import static meta.Name.*;
 import meta.*;
 import java.util.*;
 import java.util.function.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 import com.github.javaparser.ast.*;
 import com.github.javaparser.ast.type.*;
 import com.github.javaparser.ast.expr.*;
@@ -10,6 +13,8 @@ import com.github.javaparser.ast.stmt.*;
 import com.github.javaparser.ast.body.*;
 
 public class VerifyWorker {
+
+    private static final Pattern TIMES_P = Pattern.compile("times\\(([a-zA-Z0-9_$]+)\\)");
 
     private ParameterMatchingWorker paramWorker;
 
@@ -30,8 +35,27 @@ public class VerifyWorker {
         MethodCallExpr callExpr = new MethodCallExpr(new NameExpr(craft.getSubjectName()), craft.getMethodName());
         callExpr.setArguments( paramWorker.leach( craft ) );
         output[0] = new ExpressionStmt(callExpr);
-        AssignExpr countExpr = new AssignExpr(new NameExpr("times"), new IntegerLiteralExpr(1), AssignExpr.Operator.ASSIGN);
-        output[1] = new ExpressionStmt(countExpr);
+        String fact = craft.getCallMeta().getFact();
+        String verifyStrategy = "times";
+        String verifyAspect = "1";
+        if (fact == null || fact.isEmpty()) {
+            WoodLog.attach(WARNING, "Found no fact for call: " + craft.getCallMeta());
+        } else {
+            if (fact.contains("atLeastOnce")) {
+                verifyStrategy = "minTimes";
+            } else if (fact.contains("never")) {
+                verifyAspect = "0";
+            } else {
+                Matcher m = TIMES_P.matcher(fact);
+                if (m.find()) {
+                    verifyAspect = m.group(1);
+                } else {
+                    WoodLog.attach(WARNING, "Unsupported fact in call: " + craft.getCallMeta());
+                }
+            }
+        }
+        AssignExpr aspect = new AssignExpr(new NameExpr(verifyStrategy), new NameExpr(verifyAspect), AssignExpr.Operator.ASSIGN);
+        output[1] = new ExpressionStmt(aspect);
         return output;
     }
 }
