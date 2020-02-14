a84063afc92ec0c950b8388ff3ded94e7e9211e4
Oz <xad.love@gmail.com>
2020-02-14 20:19:54 +0700

    clean v1

--- src/main/java/TestTransformer/App.java
+++ src/main/java/TestTransformer/App.java
@@ -1,82 +1,24 @@
 package TestTransformer;
 
-import worker.DumpWorker;
-
 import static meta.Name.INTERRUPT_SIGNAL;
 import java.io.*;
-import subject.*;
 import worker.*;
 import storage.LineLoader;
 import storage.LibraryImplLoader;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.CountDownLatch;
-import java.util.ArrayList;
 import com.github.javaparser.ast.*;
 
 public class App {
-
-    public App() {
-        NeutralSubject.doNoop();
-        NonStaticSubject.doNoop();
-        StaticSubject.doNoop();
-    }
-
-    public int getVal() {
-        Storage.reset();
-        StaticSubject.noRefun();
-        NonStaticSubject nss = NonStaticSubject.create(1298, 9l, new ArrayList<>());
-        nss.noReturn();
-        return StaticSubject.getRefun() + nss.val() + Storage.feed + Storage.foo + nss.lift(80) + nss.lift(6);
-    }
-
-    public int fn01() {
-        return new NonStaticSubject().fval();
-    }
-
-    public int fn02() {
-        try {
-            return StaticSubject.fval();
-        } catch (Exception e) {
-            return 0;
-        }
-    }
-
-    public int fn03() {
-        NonStaticSubject s = new NonStaticSubject();
-        return s.fval() + s.sval();
-    }
-    public int fn03_1() {
-        NonStaticSubject s = StaticSubject.getNext();
-        return s.fval() + s.sval();
-    }
-    public int fn03_2() {
-        NonStaticSubject s = NonStaticSubject.create(1298, 9l, new ArrayList<>());
-        return s.fval() + s.sval();
-    }
-    public int fn04() {
-        //NonStaticSubject s = new NonStaticSubject();
-        NonStaticSubject s = StaticSubject.getNext();
-        //System.out.println("sval: " + s.sval());
-        s.reset();
-        return s.fval() + s.sval();
-    }
-
-    public int fn05() {
-        return StaticSubject.fn05();
-    }
-
-    public void fn06() {
-        StaticSubject.fn06();
-    }
 
     public static void main(String[] args) throws Exception {
         LibraryImplLoader.load();
         boolean real = true;
         if ( ! real) {
-            //String targetFile = "/zk/pMortgage/crdhway/unittest/ch/axonivy/fintech/crdhway/changeobserver/CustomSalutationDebtorChangeListenerTest.java";
-            String targetFile = "/zk/pMortgage/crdhway/unittest/ch/axonivy/fintech/crdhway/protocol/service/CrdhwayProtocolDataWrapperServiceTest.java";
+            String targetFile = "";
             CompilationUnit cUnit = new CompilationUnitWorker().transform(targetFile);
             String of = targetFile.substring(0, targetFile.length() - 5) + "Sub.java";
             try (FileWriter fw = new FileWriter(new File(of))) {
